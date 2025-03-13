23ed5532df01723b0b8e7cf75c8a14bde500e526
PipeRojas <andresf_135@hotmail.com>
2025-03-13 15:45:34 -0400

    Employee entity created

---
+++ src/main/java/edu/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package edu.getarrays.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
